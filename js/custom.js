2b6dc269faad4bb30cfd2f8de7aee97cf63aca5c
muhammadlovlu <chyshahab@gmail.com>
2021-02-18 19:44:09 +0600

    updated

--- js/custom.js
+++ js/custom.js
@@ -30,25 +30,5 @@ async function dataLoad() {
     document.getElementById("results").style.display = "block";
 }
 
-// async function dataLoad (){
-//     const res = await fetch("https://restcountries.eu/rest/v2/name/Bangladesh");
-//     const data = await res.json();
-//     const countryNameData = document.getElementById("countryName");
-//     countryNameData.innerText = data[0].name;
-//     const populationData = document.getElementById("population");
-//     populationData.innerText = data[0].population;
-//     const areaData =document.getElementById("area");
-//     areaData.innerText = data[0].area;
-//     const capitalData = document.getElementById("capital");
-//     capitalData.innerText = data[0].capital;
-//     const callingCodeData = document.getElementById("callCode");
-//     callingCodeData.innerText = data[0].callingCodes;
-//     const regionData = document.getElementById("region");
-//     regionData.innerText = data[0].region;
-//     const borderData = document.getElementById("borders");
-//     borderData.innerText = data[0].borders;
-//     const timeZoneData = document.getElementById("timezone");
-//     timeZoneData.innerText = data[0].timezones;
-//     const countryFlag = document.getElementById("flag");
-//     countryFlag.src = data[0].flag;
-// }
+
+
